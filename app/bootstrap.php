33b9223fbcf4e895d91c96e4de729a30292436d3
AlexAffter <alex.affter@gmail.com>
2015-07-09 16:07:12 +0300

    Service providers autoload added in bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,4 +8,10 @@ $app['console'] = function(){
     return new \Symfony\Component\Console\Application();
 };
 
+$providers = require_once __DIR__.'providers.php';
+
+foreach ($providers as $serviceProvider) {
+    $app->register(new $serviceProvider);
+}
+
 return $app;
